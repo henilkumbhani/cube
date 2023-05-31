bc7cebde34f7c39a51e2d87a98a1f317beaccbe3
henilkumbhani <131843236+henilkumbhani@users.noreply.github.com>
2023-05-31 15:26:14 +0530

    Add files via upload

---
+++ f1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+double cube(double num);
+
+int main()
+{
+	int num;
+	double c;
+	
+	
+	printf("enter any number: ");
+	scanf("%d", &num);
+	
+	c  =  cube(num);
+	
+	printf("cube of %d is %.2f", num, c);
+	
+	return 0;
+}
+
+
+double cube(double num)
+{
+	return (num * num * num);
+}
